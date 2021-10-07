fb64a67a0618d99a835a857a6fde1a9ab02b41ff
Bhalchandra <bvsahasrabuddhe@gmail.com>
2021-10-07 11:39:35 -0500

    feat: Polish

--- Fundamentals/src/com/bvsahasrabuddhe/CustomClass.java
+++ Fundamentals/src/com/bvsahasrabuddhe/CustomClass.java
@@ -4,6 +4,21 @@ class Employee{
     String name;
     int salary;
 
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public void printDetails() {
         System.out.println("My id is " + id);
@@ -14,17 +29,21 @@ class Employee{
 public class CustomClass {
     public static void main(String[] args) {
         Employee Bob = new Employee();
-        Employee John = new Employee();
-
-        Bob.id = 1;
-        Bob.name = "Bobby";
-        Bob.salary = 50000;
-
-        John.id = 2;
-        John.name = "Jonny";
-        John.salary = 70000;
-
-        Bob.printDetails();
-        John.printDetails();
+//        Employee John = new Employee();
+//
+//        Bob.id = 1;
+//        Bob.name = "Bobby";
+//        Bob.salary = 50000;
+//
+//        John.id = 2;
+//        John.name = "Jonny";
+//        John.salary = 70000;
+//
+//        Bob.printDetails();
+//        John.printDetails();
+        Bob.setId(19);
+        Bob.setName("Bobby");
+        System.out.println(Bob.getId());
+        System.out.println(Bob.getName());
     }
 }
