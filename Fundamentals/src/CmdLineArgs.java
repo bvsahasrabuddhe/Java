629d641cb7a3b48a548aa908885c8bbb91fcc2e3
Bhalchandra <bvsahasrabuddhe@gmail.com>
2021-10-12 13:31:25 -0500

    docs: Polish

--- Fundamentals/src/CmdLineArgs.java
+++ Fundamentals/src/CmdLineArgs.java
@@ -4,5 +4,16 @@ class CmdLineAgrs {
         System.out.println(args[0]);
         System.out.println(args[1]);
         System.out.println(args[2]);
+
+        /*
+        Command line arguments
+        D:\Learning\Java>java CmdLineAgrs Bharat Bhalchandra Pranit
+        Hello World
+        Bharat
+        Bhalchandra
+        Pranit
+
+        D:\Learning\Java>
+         */
     }
 }
