61e846ce7fbb5ab1fcfe30a5179c3407ce252008
Bhalchandra <bvsahasrabuddhe@gmail.com>
2021-10-16 15:39:58 -0500

    feat: Polish

--- Fundamentals/src/com/bvsahasrabuddhe/Solution.java
+++ Fundamentals/src/com/bvsahasrabuddhe/Solution.java
@@ -26,22 +26,23 @@ class Solution{
 
 
     public static void main(String[] args){
+
+////        byte b = 127;
+////        b++;
+////        b++;
+////        System.out.println(b);
+//        int arr[] = new int[5];
+//        System.out.println(arr);
         Scanner scan = new Scanner(System.in);
-        int number = scan.nextInt();
-        int i, prime = 1;
+        int j;
+        j = scan.nextInt();
+        int a [] = new int[j];
+        for(int i=0; i<=a.length-1; i++) {
+            System.out.println("Enter the elements");
+                a[i] = scan.nextInt();
+        }
+        System.out.println(a.length);
 
 
-        for(i = 2; i < number; i++) {
-            if((number % i) == 0) {
-                prime = 0;
-            }
-        }
-
-        if (prime == 1){
-            System.out.format("%d is prime number." , number);
-        }
-        else
-        {   System.out.format("%d is not a prime number." , number);
-        }
     }
 }
