ef2509a77a3bfc3cfabd8a10a2d5eb4f57dc5970
Bhalchandra <bvsahasrabuddhe@gmail.com>
2021-10-16 12:14:06 -0500

    feat: polish

--- Fundamentals/src/com/bvsahasrabuddhe/ArrayEx.java
+++ Fundamentals/src/com/bvsahasrabuddhe/ArrayEx.java
@@ -12,7 +12,7 @@ public class ArrayEx {
         for(int i=0; i<=a.length-1; i++) {
             for(int j=0; j<=a[i].length - 1; j++){
                 System.out.println("Enter the ages of class "+i+ "students" + j);
-                a[i][j] = scan.nextInt()    ;
+                a[i][j] = scan.nextInt();
             }
         }
         System.out.println();
