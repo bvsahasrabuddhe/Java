8f44910c58db4e3902fba79d46fb53f9141332aa
Bhalchandra <bvsahasrabuddhe@gmail.com>
2021-10-16 12:12:39 -0500

    doc: Array Example of adding ages of 2 classes and 5 students

--- Fundamentals/src/com/bvsahasrabuddhe/ArrayEx.java
+++ Fundamentals/src/com/bvsahasrabuddhe/ArrayEx.java
@@ -10,7 +10,7 @@ public class ArrayEx {
         Scanner scan = new Scanner(System.in);
 
         for(int i=0; i<=a.length-1; i++) {
-            for(int j=0; j<=a[i].length - 1; j++){10
+            for(int j=0; j<=a[i].length - 1; j++){
                 System.out.println("Enter the ages of class "+i+ "students" + j);
                 a[i][j] = scan.nextInt()    ;
             }
