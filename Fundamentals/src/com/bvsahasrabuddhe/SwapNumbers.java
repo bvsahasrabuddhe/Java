e941c17ad36de4f94eaa73808b210dbfd0c3aaac
Bhalchandra <bvsahasrabuddhe@gmail.com>
2021-10-16 16:06:59 -0500

    doc: Swap numbers

--- Fundamentals/src/com/bvsahasrabuddhe/SwapNumbers.java
+++ Fundamentals/src/com/bvsahasrabuddhe/SwapNumbers.java
@@ -1,6 +1,26 @@
 package com.bvsahasrabuddhe;
 
+import java.util.Scanner;
+
 public class SwapNumbers {
+    public static void main(String[] args) {
+        int x, y, t;// x and y are to swap
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of X and Y");
+        x = sc.nextInt();
+        y = sc.nextInt();
+        System.out.println("Before Swapping");
+        System.out.println(x);
+        System.out.println(y);
+        /*swapping */
+        t = x;
+        x = y;
+        y = t;
+        System.out.println("After Swapping");
+        System.out.println(x);
+        System.out.println(t);
 
 
+
+    }
 }
