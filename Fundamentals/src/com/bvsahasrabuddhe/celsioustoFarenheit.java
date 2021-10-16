97e9c9dc82026af7bbe8bd93ab168efdb5da6078
Bhalchandra <bvsahasrabuddhe@gmail.com>
2021-10-16 15:49:10 -0500

    feat: Polish

---
+++ Fundamentals/src/com/bvsahasrabuddhe/celsioustoFarenheit.java
@@ -0,0 +1,15 @@
+package com.bvsahasrabuddhe;
+
+import java.util.Scanner;
+
+public class celsioustoFarenheit {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        float i;
+        i = scan.nextFloat();
+
+        System.out.println((i * 9/5) + 32);
+
+    }
+}
