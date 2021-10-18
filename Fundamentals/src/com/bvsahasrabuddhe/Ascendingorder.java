b3988dc39e6855a34fee384d845af7334c8c552c
Bhalchandra <bvsahasrabuddhe@gmail.com>
2021-10-18 17:16:58 -0500

    doc: Smallest number of given Number

---
+++ Fundamentals/src/com/bvsahasrabuddhe/Ascendingorder.java
@@ -0,0 +1,27 @@
+package com.bvsahasrabuddhe;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ascendingorder {
+    public static void main(String[] args) {
+
+
+        Scanner scan = new Scanner(System.in);
+        int i, j;
+        j = scan.nextInt();
+        int a[] = new int[j];
+
+        for ( i = 0; i <= a.length - 1; i++) {
+            System.out.println("Enter the elements");
+            a[i] = scan.nextInt();
+        }
+       Arrays.sort(a) ;
+
+        for ( i = 0; i <= a.length - 1; i++) {
+            System.out.println(a[i]);
+
+        }
+
+    }
+}
