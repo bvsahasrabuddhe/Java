33a965750b72f683f4437c608609412b14d74f5d
Bhalchandra <bvsahasrabuddhe@gmail.com>
2021-10-20 11:36:18 -0500

    doc: LCM

---
+++ Fundamentals/src/com/bvsahasrabuddhe/LCM.java
@@ -0,0 +1,30 @@
+package com.bvsahasrabuddhe;
+
+import java.util.Scanner;
+
+public class LCM {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int n  = scan.nextInt();
+        int m  = scan.nextInt();
+        int min = 0;
+        if(n<m){
+            min = n;
+        }
+        else
+        {
+            min = m;
+        }
+        int gcd=0;
+        for (int i = 1; i<=min; i++)
+        {
+            if(n%i == 0 && m%i ==0)
+            {
+                gcd = i;
+            }
+        }
+
+        System.out.println((n*m)/gcd);
+    }
+}
