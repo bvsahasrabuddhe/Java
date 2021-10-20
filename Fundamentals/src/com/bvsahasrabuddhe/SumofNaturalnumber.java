948b1c5f69d0e8235b8cdc8fc05adeb0ccc42de7
Bhalchandra <bvsahasrabuddhe@gmail.com>
2021-10-20 11:57:22 -0500

    doc: Sum of n natural numbers without loop

--- Fundamentals/src/com/bvsahasrabuddhe/SumofNaturalnumber.java
+++ Fundamentals/src/com/bvsahasrabuddhe/SumofNaturalnumber.java
@@ -5,13 +5,20 @@ import java.util.Scanner;
 public class SumofNaturalnumber {
     public static void main(String[] args) {
 
+//        Scanner scan = new Scanner(System.in);
+//        int n  = scan.nextInt();
+//        int sum = 0;
+//        for (int i = 0; i<=n; i++)
+//        {
+//             sum= sum + i;
+//        }
+//        System.out.println(sum);
+
+
         Scanner scan = new Scanner(System.in);
         int n  = scan.nextInt();
-        int sum = 0;
-        for (int i = 0; i<=n; i++)
-        {
-             sum= sum + i;
-        }
+        int sum = (n *(n +1))/2;
+
         System.out.println(sum);
     }
 }
