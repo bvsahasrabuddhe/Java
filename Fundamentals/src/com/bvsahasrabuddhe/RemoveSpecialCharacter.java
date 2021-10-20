c9417ce6c377da3267cbfaf0e38a403df69d60d4
Bhalchandra <bvsahasrabuddhe@gmail.com>
2021-10-20 18:08:03 -0500

    doc: Remove Special Character in the String

---
+++ Fundamentals/src/com/bvsahasrabuddhe/RemoveSpecialCharacter.java
@@ -0,0 +1,15 @@
+package com.bvsahasrabuddhe;
+
+import java.util.Scanner;
+
+public class RemoveSpecialCharacter {
+    public static void main(String[] args) {
+
+        int counter = 0;
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+
+        s = s.replaceAll("[^a-zA-Z0-9]", "");
+        System.out.println(s);
+    }
+}
