ab5315cf539f6caa8b5e31d845d96acb4e4fff5b
Bhalchandra <bvsahasrabuddhe@gmail.com>
2021-11-17 15:32:36 -0600

    doc : Mars Exploration on Hackerrank

---
+++ Fundamentals/src/com/bvsahasrabuddhe/MarsExploration.java
@@ -0,0 +1,32 @@
+package com.bvsahasrabuddhe;
+
+import java.util.Scanner;
+
+public class MarsExploration {
+
+    static int marsExploration(String s)  {
+
+        int count = 0;
+        for (int i = 0; i < s.length(); i = i + 3) {
+            if (s.charAt(i) != 'S') {
+                count++;
+            }
+            if (s.charAt(i + 1) != 'O') {
+                count++;
+            }
+            if (s.charAt(i+2) != 'S') {
+                count++;
+            }
+
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        System.out.println(marsExploration(s));
+    }
+}
