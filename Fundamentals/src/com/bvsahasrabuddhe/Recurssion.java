3d8ebcf6a9267047338d853043535de8cc9f3b5f
Bhalchandra <bvsahasrabuddhe@gmail.com>
2021-11-23 14:28:28 +0530

    feat : Polish

--- Fundamentals/src/com/bvsahasrabuddhe/Recurssion.java
+++ Fundamentals/src/com/bvsahasrabuddhe/Recurssion.java
@@ -1,16 +1,27 @@
 package com.bvsahasrabuddhe;
 
 public class Recurssion {
-    static void fun(int n){
-        if(n<1)
-        {
-            return;
+ //   static void fun(int n){
+    static int factorial(int n){
+        if (n==0 || n ==1){
+            return 1;
         }
-        System.out.println(n);
-        fun(n-1);
-    }
+        return n* factorial(n-1);
+        }
+//        if(n<1)
+//        {
+//            return;
+//        }
+//        System.out.println(n);
+//        fun(n-1);
+   // }
 
     public static void main(String[] args) {
-        fun(3);
+//        fun(3);
+        int n = 5;
+        System.out.println(factorial(n));
+//        for(int i=3;i>=1;i--){
+//            System.out.println(i);
+//        }
     }
 }
