c68f642ff9124d59f164f9d03624044c7d05926a
Bhalchandra <bvsahasrabuddhe@gmail.com>
2021-11-24 14:34:06 +0530

    doc : Count Number Of Didits in given number

---
+++ Fundamentals/src/com/bvsahasrabuddhe/CountNumberOfDidits.java
@@ -0,0 +1,21 @@
+package com.bvsahasrabuddhe;
+
+import java.util.Scanner;
+
+public class CountNumberOfDidits {
+    static int countDidits(int n){
+        int count = 0;
+        while(n>0){
+            n = n /10;
+            count++;
+        }
+        return count;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        System.out.println(countDidits(n)) ;
+    }
+}
