91875cd50462dc6bd29d328c4f6daa75443456d8
Bhalchandra <bvsahasrabuddhe@gmail.com>
2021-11-25 13:34:28 +0530

    feat : User input added

--- Fundamentals/src/com/bvsahasrabuddhe/CountCharacter.java
+++ Fundamentals/src/com/bvsahasrabuddhe/CountCharacter.java
@@ -6,9 +6,10 @@ public class CountCharacter {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String s = scan.nextLine();
+         char ch = scan.next().charAt(0);
         int count = 0;
         for(int i=0 ; i<s.length();i++){
-            if(s.charAt(i) == 'a'){
+            if(s.charAt(i) == ch){
                 count++;
             }
         }
