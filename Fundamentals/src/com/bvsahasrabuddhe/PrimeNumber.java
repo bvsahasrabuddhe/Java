430f0104773c1b037c08557b773d1cd04ad13e86
Bhalchandra <bvsahasrabuddhe@gmail.com>
2021-12-16 14:13:11 +0530

    doc: Check Prime number

---
+++ Fundamentals/src/com/bvsahasrabuddhe/PrimeNumber.java
@@ -0,0 +1,29 @@
+package com.bvsahasrabuddhe;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+    static boolean checkPrime(int n){
+        if(n==1){
+            return false;
+        }
+        if(n==2 || n==3){
+            return true;
+        }
+        if(n%2==0 || n%3==0){
+            return false;
+        }
+        for(int i=5; i*i<=n;i=i+6){
+            if(n%i==0 || n%(i+2)==0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        System.out.println(checkPrime(n));
+    }
+}
