d2e83b23beeacc3c39c9113979eb8a37d615b5fc
Bhalchandra <bvsahasrabuddhe@gmail.com>
2021-12-16 12:38:11 +0530

    doc : Check Power of two

---
+++ Fundamentals/src/com/bvsahasrabuddhe/PowerOf2.java
@@ -0,0 +1,17 @@
+package com.bvsahasrabuddhe;
+
+import java.util.Scanner;
+
+public class PowerOf2 {
+        static boolean powerOf2(int n){
+            if(n==0){
+                return false ;
+            }
+            return  (n & (n-1)) ==0;
+        }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        System.out.println(powerOf2(n));
+    }
+}
