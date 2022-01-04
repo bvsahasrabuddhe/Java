13c6890c098e4f7e5895ac1265a49eb81837025c
Bhalchandra <bvsahasrabuddhe@gmail.com>
2022-01-04 12:01:56 +0530

    doc: Max and Min Array Sum

--- Fundamentals/src/com/bvsahasrabuddhe/MaxandMinArraySum.java
+++ Fundamentals/src/com/bvsahasrabuddhe/MaxandMinArraySum.java
@@ -12,16 +12,33 @@ public class MaxandMinArraySum {
         {
             sum = sum + a[i];
         }
-        System.out.println(sum);
+        System.out.println(sum);  // Minimum sum is 5 + 7 + 9 + 11 = 32
 
         int sum1 = 0;
         for (int i= 1; i< a.length   ; i++)
         {
             sum1 = sum1 + a[i];
         }
-        System.out.println(sum1);
+        System.out.println(sum1);   //maximum sum is 7 + 9 + 11 + 13 = 40
 
-    }
+
+        int add = 0;
+        for (int i=0; i< a.length  ; i++)
+        {
+            add = add + a[i];
+        }
+        System.out.println(add - a[a.length-1]);
+        System.out.println(add - a[0]);
+
+
+//        for (int i = 0,  j = 1; i< a.length - 1 , j < a.length ; i++, j++)
+//        {
+//            sum = sum + a[i];
+//            sum1 = sum1 + a[j];
+//        }
+//        System.out.println(sum);
+//        System.out.println(sum1);
+     }
 
 
 
