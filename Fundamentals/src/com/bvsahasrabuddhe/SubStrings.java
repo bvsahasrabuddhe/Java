1a11cc71ca1c6f1d691b542d4043289f39e69f90
Bhalchandra <bvsahasrabuddhe@gmail.com>
2022-03-10 18:35:07 +0530

    doc: Substring mississippi

--- Fundamentals/src/com/bvsahasrabuddhe/SubStrings.java
+++ Fundamentals/src/com/bvsahasrabuddhe/SubStrings.java
@@ -15,8 +15,28 @@ public class SubStrings {
             }
         }
     }
+
+    static void printAllSubstrings1(String st, int k){
+        int n = st.length();
+
+            for(int s =0 ; s <= n - k ; s++){
+                for(int e =s ; e <= (s+k)-1; e++){
+                    System.out.print(st.charAt(e));
+                }
+                System.out.println();
+            }
+
+    }
     public static void main(String[] args) {
-         String s   = "abcde";
-        printAllSubstrings(s);
+//        String s   = "abcde";
+//        printAllSubstrings(s);
+        String s   = "abcde";
+        printAllSubstrings1(s, 2);
+//        Out put is
+//        ab
+//        bc
+//        cd
+//        de
+
     }
 }
